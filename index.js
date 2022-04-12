83dcc3d3c2c5d13d3b901f5dadcaea32442357c0
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-04-12 23:50:05 +0300

    Update pdf generator and http handlers

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 const puppeteer = require("puppeteer")
 const express = require("express")
 
-const errorConvertingHtmlToPdf = {"error": "Error converting html to pdf"}
+class PDFGenerator {
+    constructor(page) {
+        this.page = page
+    }
 
-async function printfPDF(htmlContent) {
-    const browser = await puppeteer.launch({
+    async generate(html, options = { format: "a4" }) {
+        await this.page.setContent(html)
+        return await this.page.pdf(options)
+    }
+}
+
+async function createBrowser() {
+    return await puppeteer.launch({
         headless: true,
         args: ['--no-sandbox']
     })
-    const page = await browser.newPage()
-
-    await page.setContent(htmlContent)
-
-    const pdf = await page.pdf({ format: "a4" })
-    await browser.close()
-
-    return pdf
 }
 
-const app = express()
+const generatePDFWithHTMLContent = (pdfGenerator) => {
+    return (req, res) => {
+        const html = req.body.html
+        const options = req.body.options || { format: "a4" }
 
-app.get("/", async (req, res) => {
-    const content = req.query["content"]
+        return generateAndSendPDF(pdfGenerator, html, options, res)
+    }
+}
 
-    printfPDF(content).then(pdf => {
-        res.set({ "Content-Type": "application/pdf", "Content-Length": pdf.length })
-        res.send(pdf)
-    }).catch(err => {
-        res.status(500).send(errorConvertingHtmlToPdf)
-    })
-})
+const generatePDFWithHTMLFile = (pdfGenerator) => {
+    return (req, res) => {
+        const htmlFile = req.body.htmlFile
+        const options = req.body.options || { format: "a4" }
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+        return generateAndSendPDF(pdfGenerator, htmlFile, options, res)
+    }
+}
+
+const generateAndSendPDF = (pdfGenerator, html, options, res) => {
+    pdfGenerator.generate(html, options)
+        .then(pdf => {
+            res.setHeader("Content-Type", "application/pdf")
+            res.setHeader("Content-Length", pdf.length)
+            res.send(pdf)
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(err)
+        })
+}
+
+// initialization
+const init = async () => {
+    const app = express()
+
+    app.use(express.json())
+
+    const browser = await createBrowser()
+    const page = await browser.newPage()
+    page.setJavaScriptEnabled(false)
+
+    const pdfGenerator = new PDFGenerator(page)
+
+    app.post("/", generatePDFWithHTMLContent(pdfGenerator))
+    app.post("/pdf", generatePDFWithHTMLFile(pdfGenerator))
+
+    app.listen(3000, () => console.log("Listening on port 3000"))
+}
+init()
